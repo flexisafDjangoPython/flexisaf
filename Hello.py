62b0ca133968f86f6d3e364633ddbb526f43a079
mujaheed21 <mujaheed21@gmail.com>
2015-07-23 12:23:55 +0100

    delted timtec

--- Hello.py
+++ Hello.py
@@ -2,6 +2,6 @@ __author__ = 'Feedah'
 
 print "Welcome"
 print "This is another line of code"
-x = ['ibro', 'tim', 'rufai', 'bello', 'mujahid'hassan]
+x = ['ibro', 'tim', 'rufai', 'bello', 'mujahid' ,'usman aliy']
 for each_line in x:
   print each_line
